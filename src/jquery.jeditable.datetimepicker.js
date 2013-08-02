b0f6fd86877ea2129036482fb8a1324272545ce1
stevro <stev.matei@gmail.com>
2013-08-02 17:08:40 +0300

    Create jquery.jeditable.datetimepicker.js
    I modified the original version of jeditable/datepicker to allow the use of datetimepicker. If you think it's ok then please make it public as others might be looking for it.
    Thanks,
    Stev

---
+++ src/jquery.jeditable.datetimepicker.js
@@ -0,0 +1,65 @@
+/*
+ * Datetimepicker for Jeditable
+ *
+ * Copyright (c) 2011 Piotr 'Qertoip' Włodarek
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * Depends on jQuery UI Datetimepicker
+ *
+ * Project home:
+ *   http://github.com/qertoip/jeditable-datepicker
+ *
+ *
+ * Contributed: Stefan Matei (2013) - stev.matei@gmail.com
+ *
+ * Now you can use datetimepicker instead of datepicker
+ *
+ *
+ */
+
+// add :focus selector
+jQuery.expr[':'].focus = function( elem ) {
+  return elem === document.activeElement && ( elem.type || elem.href );
+};
+
+$.editable.addInputType( 'datetimepicker', {
+
+    /* create input element */
+    element: function( settings, original ) {
+      var form = $( this ),
+          input = $( '<input />' );
+      input.attr( 'autocomplete','off' );
+      form.append( input );
+      return input;
+    },
+    
+    /* attach jquery.ui.datepicker to the input element */
+    plugin: function( settings, original ) {
+      var form = this,
+          input = form.find( "input" );
+
+      // Don't cancel inline editing onblur to allow clicking datepicker
+      settings.onblur = 'nothing';
+      
+      datetimepicker = {        
+        
+        onClose: function() {
+          setTimeout( function() {
+                          
+            form.submit();
+            // the delay is necessary; calendar must be already
+            // closed for the above :focus checking to work properly;
+            // without a delay the form is submitted in all scenarios, which is wrong
+          }, 150 );
+        }
+      };
+    
+      if (settings.datetimepicker) {
+        jQuery.extend(datetimepicker, settings.datetimepicker);
+      }
+
+      input.datetimepicker(datetimepicker);
+    }
+} );
